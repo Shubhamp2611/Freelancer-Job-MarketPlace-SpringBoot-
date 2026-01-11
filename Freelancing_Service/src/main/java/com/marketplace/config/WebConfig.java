4ba0cb2a522cf44b2bdc5f795c70ecf3998a394f
Shubham patil <shree22kar2411@gmail>
2026-01-11 15:28:29 +0530

    deploy2.0

--- Freelancing_Service/src/main/java/com/marketplace/config/WebConfig.java
+++ Freelancing_Service/src/main/java/com/marketplace/config/WebConfig.java
@@ -1,16 +1,27 @@
 package com.marketplace.config;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jakarta.annotation.PostConstruct;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     
-    @Value("${app.file.upload-dir}")
-    private String uploadDir;
+	@Value("${app.file.upload-dir:/tmp/uploads}")
+	private String uploadDir;
+
     
+	@PostConstruct
+	public void init() {
+	    new File(uploadDir).mkdirs();
+	}
+
+	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
